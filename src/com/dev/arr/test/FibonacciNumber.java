534f12aa969a66038587f7203c08a863d93e5f59
satishlabs <spd61185@gmail.com>
2024-12-05 22:33:27 +0530

    Fibonacci numbers

---
+++ src/com/dev/arr/test/FibonacciNumber.java
@@ -0,0 +1,21 @@
+package com.dev.arr.test;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        FibonacciNumber fibonacciNumber = new FibonacciNumber();
+        fibonacciNumber.fibonacciNumber(10);
+    }
+
+private void fibonacciNumber(int n) {
+    int firstNum = 0;
+    int secondNum = 1;
+    System.out.print("Fibonacci Series up to " + n + " numbers: ");
+
+    for (int i = 1; i <= n; i++) {
+        System.out.print(firstNum + " ");
+        int nextNum = firstNum + secondNum;
+        firstNum = secondNum;
+        secondNum = nextNum;
+    }
+}
+}
