f3ee4d05ff293efba5b5faebfd24679f17e30723
satishlabs <spd61185@gmail.com>
2024-12-10 07:04:43 +0530

    Find Max Sum Sub Array

---
+++ src/com/dev/arr/MaxSumSubArray.java
@@ -0,0 +1,33 @@
+package com.dev.arr;
+
+public class MaxSumSubArray {
+    public static void main(String[] args) {
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        //output: 4+(-1)+2+1=6
+        int res = maxSumSubArray(arr);
+        System.out.println(res);
+    }
+
+    private static int maxSumSubArray(int[] arr) {
+        if(arr.length==1)
+            return arr[0];
+
+        Integer max = Integer.MIN_VALUE;
+        Integer sum = 0;
+
+        int startIndex = 0;
+        int endIndex = arr.length-1;
+
+        while(startIndex < endIndex){
+            sum = sum + arr[startIndex++];
+
+            if(max < sum){
+                max = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
