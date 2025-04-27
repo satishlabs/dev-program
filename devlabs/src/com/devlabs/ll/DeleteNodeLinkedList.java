42ca3c4f97a665fdd9e0af7ae32688dff9cf994c
satishlabs <spd61185@gmail.com>
2025-04-27 20:37:54 +0530

    Delete Node in linkedlist by position

--- devlabs/src/com/devlabs/ll/DeleteNodeLinkedList.java
+++ devlabs/src/com/devlabs/ll/DeleteNodeLinkedList.java
@@ -16,9 +16,43 @@ public class DeleteNodeLinkedList {
         list.deleteByValue(3);
         System.out.println("\nList after deleting 3:");
         list.printList();
+        
+        // Delete node at position 2 (0-based), i.e., 3
+        list.deleteByPosition(2);
+        System.out.println("\nAfter deleting node at position 2:");
+        list.printList();
 	}
 	
-	 // Function to delete a node by value
+	// Delete node at a given position
+	 private void deleteByPosition(int position) {
+		if(head == null) {
+			System.out.println("List is empty...");
+			return;
+		}
+		
+		Node temp = head;
+		//if head needs to be removed
+		if(position == 0) {
+			head = temp.next;
+			return;
+		}
+		
+		// Find previous node of the node to be deleted
+		for(int i=0; temp != null && i < position-1; i++) {
+			temp = temp.next;
+		}
+		
+		// If position is more than number of nodes
+		if(temp == null || temp.next == null) {
+			System.out.println("Position is out of buds...");
+			return;
+		}
+		
+		 // Node temp.next is the node to delete
+		temp.next = temp.next.next;
+	}
+
+	// Function to delete a node by value
 	private void deleteByValue(int key) {
 		if(head == null) {
 			System.out.println("List is empty...");
