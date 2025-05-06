1917bf707480758082bd862e5522678951004540
satishlabs <spd61185@gmail.com>
2025-05-06 08:30:40 +0530

    remove duplicates

---
+++ devlabs/src/com/devlabs/interviews/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package com.devlabs.interviews;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+		String input = "programming";
+        String output = removeDuplicates(input);
+
+        System.out.println("Original: " + input);
+        System.out.println("Without duplicates: " + output);
+	}
+
+	private static String removeDuplicates(String input) {
+		if(input == null) {
+			return null;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		Set<Character> set = new LinkedHashSet<Character>();
+		for(Character ch: input.toCharArray()) {
+			if(!set.contains(ch)) {
+				set.add(ch);
+				sb.append(ch);
+			}
+		}
+		
+		return sb.toString();
+	}
+}
