780c80cb30c9c1b9df4b42ca0010a72259a6e477
satishlabs <spd61185@gmail.com>
2025-05-13 17:36:01 +0530

    Revered each word

--- devlabs/src/com/devlabs/interviews/ReverseWords.java
+++ devlabs/src/com/devlabs/interviews/ReverseWords.java
@@ -7,6 +7,24 @@ public class ReverseWords {
 
         System.out.println("Original sentence: " + input);
         System.out.println("Reversed sentence: " + result);
+        System.out.println("\n============================");
+        String output = reverseWordsInPlace(input);
+
+        System.out.println("Original: " + input);
+        System.out.println("Reversed words in place: " + output);
+	}
+
+	private static String reverseWordsInPlace(String input) {
+		if(input == null || input.trim().isEmpty()) {
+			return input;
+		}
+		
+		String[] words = input.split("\\s+");
+		StringBuilder sb = new StringBuilder();
+		for(String word : words) {
+			sb.append(new StringBuilder(word).reverse().append(" "));
+		}
+		return sb.toString().trim();
 	}
 
 	private static String reverseWords(String input) {
