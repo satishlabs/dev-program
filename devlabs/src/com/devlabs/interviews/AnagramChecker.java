2d2e9fc9dac4373fa555262923549378773568f9
satishlabs <spd61185@gmail.com>
2025-05-15 18:46:54 +0530

    updated format

--- devlabs/src/com/devlabs/interviews/AnagramChecker.java
+++ devlabs/src/com/devlabs/interviews/AnagramChecker.java
@@ -4,10 +4,10 @@ public class AnagramChecker {
 	public static void main(String[] args) {
 		String str1 = "Listen";
 		String str2 = "Silent";
-		if(areAnagrams(str1,str2)) {
-			System.out.println(str1+" and "+str2+" are anagram.");
-		}else {
-			System.out.println(str1+" and "+str2+" are not anagram.");
+		if (areAnagrams(str1, str2)) {
+			System.out.println(str1 + " and " + str2 + " are anagram.");
+		} else {
+			System.out.println(str1 + " and " + str2 + " are not anagram.");
 		}
 	}
 
@@ -15,20 +15,20 @@ public class AnagramChecker {
 		// Remove spaces and convert to lowercase
 		str1 = str1.replaceAll("\\s+", "").toLowerCase();
 		str2 = str2.replaceAll("\\s+", "").toLowerCase();
-		
-		if(str1.length() != str2.length()) {
+
+		if (str1.length() != str2.length()) {
 			return false;
 		}
-		
-		int[] counts = new int[26]; //for a to z
-		
-		for(int i =0; i< str1.length(); i++) {
-			counts[str1.charAt(i) - 'a']++; //increment for str1;
-			counts[str2.charAt(i) - 'a']--; //increment for str2
+
+		int[] counts = new int[26]; // for a to z
+
+		for (int i = 0; i < str1.length(); i++) {
+			counts[str1.charAt(i) - 'a']++; // increment for str1;
+			counts[str2.charAt(i) - 'a']--; // increment for str2
 		}
-		 // If all counts are zero, they are anagrams
-		for(int count : counts) {
-			if(count != 0)
+		// If all counts are zero, they are anagrams
+		for (int count : counts) {
+			if (count != 0)
 				return false;
 		}
 		return true;
