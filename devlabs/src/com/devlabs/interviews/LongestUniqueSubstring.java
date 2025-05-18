b306c88d76f0afb700bd48d1a7be543bc78f7d42
satishlabs <spd61185@gmail.com>
2025-05-18 22:41:18 +0530

    LongestSubsttring

---
+++ devlabs/src/com/devlabs/interviews/LongestUniqueSubstring.java
@@ -0,0 +1,31 @@
+package com.devlabs.interviews;
+
+public class LongestUniqueSubstring {
+	public static void main(String[] args) {
+		String input = "abcabcbb";
+		int length = lengthOfLongestSubstring(input);
+		System.out.println("Length of Longest Substring Without Repeating Characters: " + length);
+	}
+
+	private static int lengthOfLongestSubstring(String input) {
+		int index[] = new int[256]; // ASCII character map
+		for (int i = 0; i < 256; i++) {
+			index[i] = -1; // initialize all characters as not seen
+		}
+
+		int maxLen = 0;
+		int start = 0; // left end of the window
+		for (int end = 0; end < input.length(); end++) {
+			char current = input.charAt(end);
+
+			if (index[current] >= start) {
+				// character already seen in current window
+				start = index[current] + 1;
+			}
+
+			index[current] = end; // update last seen index
+			maxLen = Math.max(maxLen, end - start + 1);
+		}
+		return maxLen;
+	}
+}
